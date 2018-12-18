45e5ffca4101de80cdca4b9ebed56b1b7b56221f
Nakya Santini <nakya.santini@tes.co.id>
2018-12-18 11:55:35 +0700

    Added vuetify

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import jquery from "jquery"
 
+import './plugins/vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "jquery/dist/jquery.min.js"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
