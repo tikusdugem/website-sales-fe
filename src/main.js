8cc6df728d33f17798cd6282c2873cc11933736a
Nakya Santini <nakya.santini@tes.co.id>
2018-12-18 16:25:44 +0700

    Remove Bootstrap and JQuery

--- src/main.js
+++ src/main.js
@@ -2,17 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import jquery from "jquery"
 
 import './plugins/vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "jquery/dist/jquery.min.js"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-
-window.$ = jquery;
 Vue.config.productionTip = false
 
 new Vue({
