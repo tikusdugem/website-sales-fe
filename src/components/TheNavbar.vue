f9e0452b29983aa26bb0d5ee5a508b5e89e4f3b4
Nakya Santini <nakya.santini@tes.co.id>
2018-12-20 21:20:05 +0700

    Fix rules Vuetify <v-app>

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,22 +1,20 @@
 <template>
-    <v-app>
-        <v-toolbar dark color="primary">
-            <v-img :src="require('@/assets/logo.svg')" max-width="30"></v-img>
-            <v-toolbar-title>othem</v-toolbar-title>
-            <v-toolbar-items class="hidden-sm-and-down ml-2">
-                <v-btn to="/home" exact flat>Home</v-btn>
-                <v-btn to="/users" exact flat>Users Management</v-btn>
-            </v-toolbar-items>
-            <v-toolbar-items class="hidden-md-and-up ml-2">
-                <v-btn to="/home" exact icon>
-                    <v-icon>home</v-icon>
-                </v-btn>
-                <v-btn to="/users" exact icon>
-                    <v-icon>people</v-icon>
-                </v-btn>
-            </v-toolbar-items>
-            <v-spacer></v-spacer>
-            <v-btn depressed color="error">Logout</v-btn>
-        </v-toolbar>
-    </v-app>
+    <v-toolbar dark color="primary">
+        <v-img :src="require('@/assets/logo.svg')" max-width="30"></v-img>
+        <v-toolbar-title>othem</v-toolbar-title>
+        <v-toolbar-items class="hidden-sm-and-down ml-2">
+            <v-btn to="/home" exact flat>Home</v-btn>
+            <v-btn to="/users" exact flat>Users Management</v-btn>
+        </v-toolbar-items>
+        <v-toolbar-items class="hidden-md-and-up ml-2">
+            <v-btn to="/home" exact icon>
+                <v-icon>home</v-icon>
+            </v-btn>
+            <v-btn to="/users" exact icon>
+                <v-icon>people</v-icon>
+            </v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-btn depressed color="error">Logout</v-btn>
+    </v-toolbar>
 </template>
